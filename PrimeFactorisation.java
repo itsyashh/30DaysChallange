42f55dc99b7c4c048aa943a2323979d49bd7f089
Yashwant Kumar <53390920+itsyashh@users.noreply.github.com>
2020-10-01 23:42:26 +0530

    Add files via upload

---
+++ PrimeFactorisation.java
@@ -0,0 +1,20 @@
+package ThirtyDaysOfCode;
+
+import java.util.Scanner;
+
+public class PrimeFactorisation {
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        for(int div = 2; div * div <= n; div++){
+            while (n % div == 0){
+                n = n / div;
+                System.out.print(div + " ");
+            }
+        }
+        if(n != 1){
+            System.out.println(n);
+        }
+    }
+}
