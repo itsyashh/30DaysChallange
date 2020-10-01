f91b3781a35b6eb65d7ba16b1218f184ece19d50
Yashwant Kumar <53390920+itsyashh@users.noreply.github.com>
2020-10-01 23:41:52 +0530

    Add files via upload

---
+++ DigitsNumbers.java
@@ -0,0 +1,25 @@
+package ThirtyDaysOfCode;
+import java.util.Scanner;
+
+public class DigitsNumbers {
+    public static void main(String [] args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int dig = 0;
+        int temp = n;
+        while(temp != 0){
+            temp = temp / 10;
+            dig++;
+
+        }
+        int div = (int)Math.pow(10, dig - 1);
+        while (div != 0){
+            int q = n / div;
+            System.out.println(q);
+
+            n = n % div;
+            div = div / 10;
+
+        }
+    }
+}
